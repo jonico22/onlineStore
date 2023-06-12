613c533c6d8981a99b864fd69f5e1ba85d89421b
Jose Luis <joseyananicolas@gmail.com>
2023-06-12 18:54:44 -0500

    feat: configuracion next-auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,48 @@
+import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { dbUsers } from '@/database';
+
+export const authOptions: NextAuthOptions = {
+  pages: {
+    signIn: "/auth/login",
+  },
+  session: {
+    maxAge: 2592000, /// 30d
+    strategy: 'jwt',
+    updateAge: 86400,
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Sign in",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          return null;
+        }
+        return await dbUsers.checkUserEmailPassword(credentials!.email, credentials!.password );
+      },
+    }),
+  ],
+  callbacks: {
+    session: ({ session, token }) => {
+      session.accessToken = token.accessToken;
+      session.user = token.user as any;
+      return session;
+    },
+    jwt: ({ token,account, user }) => {
+
+      if ( account ) {
+        token.accessToken = account.access_token;
+        token.user = user;
+      }
+      return token;
+    },
+  },
+};
